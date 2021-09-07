20ab0136dc979fa3d85087e8f13d37b0a6abdd6c
judyi19997 <judyi1999@kookmin.ac.kr>
2021-09-08 03:57:55 +0900

    download .txt data

--- ONEGO/app.js
+++ ONEGO/app.js
@@ -14,10 +14,9 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const modelRouter = require('./routes/model_server');
 const awsRouter = require('./routes/aws');
-// const spellRouter = require('./routes/spell_check');
+const spellRouter = require('./routes/spell_check');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -32,7 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/model',modelRouter);
 app.use('/aws',awsRouter);
-// app.use('/spell',spellRouter);
+app.use('/spell',spellRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- ONEGO/routes/model_server.js
+++ ONEGO/routes/model_server.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const request = require('request');
+// const string = require(String);
 const router = express.Router();
+const fs = require('fs');
+const { type } = require('os');
 
 router.get('/',(req,res)=>{
     res.send('model라우터');
 })
 
+// String.prototype.replaceAll = function(org, dest) {
+//     return this.split(org).join(dest);
+// }
 
 router.get('/test/:uid', function (req, res, next) {
     let UID = req.params.uid
     const testAPI = (callback)=>{ 
         const options = {
             method: 'GET',
-            uri: `http://127.0.0.1:5000/test?uid=${UID}`,
+            uri: `http://127.0.0.1:5000/file_download?uid=${UID}`,
         }
 
         request(options, function (err, res, body) {
-            console.log(res)
+            console.log('res : '+ res + typeof(res))
+            console.log('body : ' + body + typeof(body))
             callback(undefined, {
-                result:body
+                result:body,
             });
         });
     }
@@ -32,13 +39,24 @@ router.get('/test/:uid', function (req, res, next) {
             });
         }
         let stringData = JSON.stringify(result);
-        res.send({
-            message: "from flask",
-            status: "success",
-            data: stringData    
-        });
+        console.log('result : '+result);
+        console.log({
+                message: "from flask",
+                status: "success", 
+                fulldata : stringData,   
+            }) 
+        if(stringData.includes('complete')){
+            res.redirect(`../../aws/download_txt/${UID}`)
+        }
+        else{
+            res.send({
+                message: "flask is fail",
+                status: "fail"
+            });
+        }
     })
 
 });
+
 
 module.exports = router;
